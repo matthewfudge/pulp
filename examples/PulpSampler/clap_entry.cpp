ffcebbd8dde1761057e08dac25317cf674cc0c23
Dan Raffel <daniel.raffel@gmail.com>
2026-03-25 20:09:24 -0700

    Fix PulpSampler CLAP entry include path (clap_entry.hpp not clap_macros.hpp)

--- examples/PulpSampler/clap_entry.cpp
+++ examples/PulpSampler/clap_entry.cpp
@@ -1,4 +1,6 @@
-#include <pulp/format/clap_macros.hpp>
+// PulpSampler CLAP entry point
+
 #include "pulp_sampler.hpp"
+#include <pulp/format/clap_entry.hpp>
 
 PULP_CLAP_PLUGIN(pulp::examples::create_pulp_sampler)
