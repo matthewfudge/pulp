39b21d78c044adf22f1bd50497e32e924eaf0e24
Pulp Test <pulp-test@example.com>
2026-05-18 11:28:26 -0700

    test(format): cover iOS background descriptor copies

--- test/test_ios_background_audio_flag.cpp
+++ test/test_ios_background_audio_flag.cpp
@@ -26,3 +26,25 @@ TEST_CASE("ios_requires_background_audio is settable per plugin",
     live_synth.ios_requires_background_audio = true;
     REQUIRE(live_synth.ios_requires_background_audio);
 }
+
+TEST_CASE("ios_requires_background_audio survives descriptor copies",
+          "[ios][descriptor][coverage][phase3]") {
+    PluginDescriptor live_looper;
+    live_looper.name = "LiveLooper";
+    live_looper.category = PluginCategory::Effect;
+    live_looper.ios_requires_background_audio = true;
+
+    PluginDescriptor copied = live_looper;
+    REQUIRE(copied.name == "LiveLooper");
+    REQUIRE(copied.category == PluginCategory::Effect);
+    REQUIRE(copied.ios_requires_background_audio);
+
+    PluginDescriptor assigned;
+    assigned = copied;
+    REQUIRE(assigned.name == "LiveLooper");
+    REQUIRE(assigned.ios_requires_background_audio);
+
+    copied.ios_requires_background_audio = false;
+    REQUIRE_FALSE(copied.ios_requires_background_audio);
+    REQUIRE(assigned.ios_requires_background_audio);
+}
