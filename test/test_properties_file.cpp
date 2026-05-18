6315b27182273403959959ad960a9087d9a2cc61
Pulp Test <pulp-test@example.com>
2026-05-18 09:31:44 -0700

    test(state): cover mixed properties string escapes

--- test/test_properties_file.cpp
+++ test/test_properties_file.cpp
@@ -401,6 +401,20 @@ TEST_CASE("PropertiesFile save and reload preserves tab and carriage escapes",
     REQUIRE(reloaded.get_string("control").value_or("") == "left\tright\rend");
 }
 
+TEST_CASE("PropertiesFile round-trips mixed JSON string escapes",
+          "[state][properties][coverage]") {
+    TemporaryFile tmp(".json");
+    const std::string value = "path\\\\plugin\t\"quoted\"\nnext line";
+
+    PropertiesFile props;
+    props.set_string("escaped", value);
+    REQUIRE(props.save(tmp.path_string()));
+
+    PropertiesFile reloaded;
+    REQUIRE(reloaded.load(tmp.path_string()));
+    REQUIRE(reloaded.get_string("escaped").value_or("") == value);
+}
+
 TEST_CASE("PropertiesFile set_path can clear the implicit save destination",
           "[state][properties][coverage][phase3-large]") {
     TemporaryFile tmp(".json");
